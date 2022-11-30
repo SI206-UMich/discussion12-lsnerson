188c93a876fab0acfc25b979d51aad4a4673362b
Lauren Snerson <laurensnerson@MacBook-Air-210.local>
2022-11-30 11:56:19 -0500

    final

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,11 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS employees")
+    cur.execute("CREATE TABLE employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, \
+        last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER)")
+    conn.commit()
+
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,16 +31,37 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    json_data = json.loads(file_data)
+
+    count = 1   
+    for i in json_data:
+        employee_id = count
+        first_name = i['first_name']
+        last_name = i['last_name']
+        hire_date = i['hire_date']
+        job_id = int(i['job_id'])
+        salary = int(i['salary'])
+        cur.execute("INSERT INTO employees (employee_id, first_name, last_name, hire_date, job_id, salary) \
+        VALUES (?,?,?,?,?,?)", (employee_id, first_name, last_name, hire_date, job_id, salary))
+        count += 1
+    conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute(f"SELECT employees.hire_date, jobs.job_title FROM employees JOIN jobs ON employees.job_id == jobs.job_id ORDER BY employees.hire_date")
+    rows = cur.fetchall()
+    return rows[0][1]
+
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.execute(f"SELECT employees.first_name, employees.last_name FROM employees JOIN jobs ON employees.job_id == jobs.job_id WHERE employees.salary > jobs.max_salary OR employees.salary < jobs.min_salary")
+    rows = cur.fetchall()
+    return rows
+
+# SELECT types from Pokemon JOIN types ON Pokemon.type_id = types.type_id
+
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
